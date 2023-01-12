0c047a8070bef7b40339933f2df346379b30c51f
24Caleb Rickenbach <24CRickenbach@htsp.Local>
2023-01-12 14:57:55 -0700

    Still needs improvement

--- src/Player.cpp
+++ src/Player.cpp
@@ -4,11 +4,11 @@
 
 #include "Player.h"
 #include <iostream>
-Player::Player(Level* playerLevelPtr) : m_speed{400}, m_gravity{2000}, m_LevelPtr{playerLevelPtr}, m_jumpForce{700}, m_isOnGround{false}{
+Player::Player(Level* playerLevelPtr) : m_speed{100}, m_gravity{1000}, m_LevelPtr{playerLevelPtr}, m_jumpForce{700}, m_isOnGround{true}{
     m_playerShape.setSize(sf::Vector2f(WORLD_SCALE,WORLD_SCALE));
     m_playerShape.setFillColor(sf::Color::Blue);
     m_playerShape.setOrigin(m_playerShape.getSize().x/2, m_playerShape.getSize().y/2);
-    m_playerShape.setPosition(200, 200);
+    m_playerShape.setPosition(200, 500);
 
 }
 
@@ -27,13 +27,18 @@ void Player::managePlayer() {
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && m_isOnGround){
         jump();
     }
+    //m_isOnGround = false;
     for(const auto& i : m_LevelPtr->getLevel()) {
         //TODO: Create collision function
         handleCollisionWithObject(i);
     }
-    
+    //In GD every jump the player rotates 180
+    m_isOnGround = m_velocity.y == 0;
 
-
+    if(!m_isOnGround){
+        //m_playerShape.rotate(180.f/(((m_jumpForce)/(m_gravity*m_dt))));
+        //m_playerShape.rotate(87.5f/((m_velocity.y/(m_gravity*m_dt))));
+    }
 }
 
 const sf::Drawable &Player::getPlayerDrawable() {
@@ -54,25 +59,25 @@ const sf::Transformable &Player::getPlayerTransformable() {
 
 bool Player::handleCollisionWithObject(LevelElement* collisionObject) {
     if (m_playerShape.getGlobalBounds().intersects(collisionObject->getGlobalBounds())){
-        if(m_playerShape.getPosition().y < collisionObject->getGlobalBounds().top && m_velocity.y > 0) {
+        if( m_velocity.y > 0) {
             m_velocity.y = 0;
             m_playerShape.setPosition(m_playerShape.getPosition().x,
-                                      collisionObject->getGlobalBounds().top - WORLD_SCALE / 2);
-            m_isOnGround = true;
-        }else if(m_playerShape.getPosition().x >= collisionObject->getGlobalBounds().left){
+                                      collisionObject->getGlobalBounds().top - WORLD_SCALE/2);
+        }else if(m_playerShape.getPosition().x + WORLD_SCALE/2 >= collisionObject->getGlobalBounds().left){
             m_velocity.x = 0;
-        }else if(m_velocity.y <= 0  ){//m_playerShape.getPosition().y >= collisionObject->getGlobalBounds().top + collisionObject->getGlobalBounds().height - WORLD_SCALE/2 &&
-            m_velocity.y = -m_velocity.y;
+        }else if(m_velocity.y <= 0){//m_playerShape.getPosition().y >= collisionObject->getGlobalBounds().top + collisionObject->getGlobalBounds().height - WORLD_SCALE/2 &&
+            m_velocity.y = 0;
             m_playerShape.setPosition(m_playerShape.getPosition().x,
-                                      collisionObject->getGlobalBounds().top + collisionObject->getGlobalBounds().height + WORLD_SCALE/2);
+                                      collisionObject->getGlobalBounds().top + 100);
             std::cout<<"Hit ceiling\n";
         }
 
         if (collisionObject->getElementType() == 1) {
             die();
         }
+
         return true;
+
     }
-    m_isOnGround = false;
     return false;
 }
